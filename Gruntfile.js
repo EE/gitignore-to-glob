ab2ac25472f4b2cf9f128b11c8689b8498510244
Michał Gołębiowski <m.goleb@gmail.com>
2015-09-19 23:14:56 +0200

    Dump JSCS in favor of ESLint, ES6-ify the code base
    The new approach is to use only these parts of ES6 that are available in
    Node 4 by default and transpile only for Node 0.10-0.12. In this way debugging
    is easier in the newest stable Node but the module can still be used in
    (some) older ones.

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,10 +8,99 @@
 
 'use strict';
 
+// Disable options that don't work in Node.js 0.12.
+// Gruntfile.js & tasks/*.js are the only non-transpiled files.
+/* eslint-disable no-var, object-shorthand, prefer-arrow-callback, prefer-const,
+ prefer-spread, prefer-reflect, prefer-template */
+
+var assert = require('assert');
+
+var newNode;
+try {
+    assert.strictEqual(eval('(() => 2)()'), 2); // eslint-disable-line no-eval
+    newNode = true;
+} catch (e) {
+    newNode = false;
+}
+
 module.exports = function (grunt) {
     require('time-grunt')(grunt);
 
     grunt.initConfig({
+        clean: {
+            all: {
+                src: [
+                    'dist',
+                    '*.log',
+                    'test/*/*-copy',
+                ],
+            },
+        },
+
+        copy: {
+            all: {
+                files: [
+                    {
+                        expand: true,
+                        dot: true,
+                        src: [
+                            'test/**/*',
+                            '!test/**/*.js',
+                        ],
+                        dest: 'dist',
+                    },
+                ],
+            },
+        },
+
+        babel: {
+            options: {
+                sourceMap: 'inline',
+                sourceRoot: __dirname,
+                retainLines: true,
+
+                whitelist: [
+                    // If a comment doesn't indicate otherwise, all commented out transformers
+                    // would transpile features not available in latest stable Node.js yet
+                    // so we can't use them as we don't transpile in latest Node.
+
+                    'es6.arrowFunctions',
+                    'es6.blockScoping',
+                    'es6.classes',
+                    'es6.constants',
+//                    'es6.destructuring',
+                    'es6.forOf',
+//                    'es6.modules',
+//                    'es6.parameters',
+                    'es6.properties.computed',
+                    'es6.properties.shorthand',
+//                    'es6.spread',
+//                    'es6.tailCall',
+                    'es6.templateLiterals',
+//                    'es6.regex.unicode',
+//                    'es6.regex.sticky',
+                    'strict',
+                ],
+
+                loose: [
+                    // Speed up for-of on arrays by not using the iterator directly.
+                    'es6.forOf',
+                ],
+            },
+            all: {
+                files: [
+                    {
+                        expand: true,
+                        src: [
+                            'lib/**/*.js',
+                            'test/**/*.js',
+                        ],
+                        dest: 'dist',
+                    },
+                ],
+            },
+        },
+
         eslint: {
             all: {
                 src: [
@@ -22,26 +111,12 @@ module.exports = function (grunt) {
             },
         },
 
-        jscs: {
-            all: {
-                src: [
-                    'Gruntfile.js',
-                    'lib/**/*.js',
-                    'test/*.js',
-                    'test/**/*.js',
-                ],
-                options: {
-                    config: '.jscsrc',
-                },
-            },
-        },
-
         mochaTest: {
             all: {
                 options: {
                     reporter: 'spec',
                 },
-                src: ['test/spec.js'],
+                src: [newNode ? 'test/spec.js' : 'dist/test/spec.js'],
             },
         },
     });
@@ -51,13 +126,21 @@ module.exports = function (grunt) {
 
     grunt.registerTask('lint', [
         'eslint',
-        'jscs',
+    ]);
+
+    // In modern Node.js we just use the non-transpiled source as it makes it easier to debug;
+    // in older version we transpile (but keep the lines).
+    grunt.registerTask('build', [
+        'copy',
+        'babel',
     ]);
 
     grunt.registerTask('test', ['mochaTest']);
 
     grunt.registerTask('default', [
+        'clean',
         'lint',
+        'build',
         'test',
     ]);
 };
--- test/spec.js
+++ test/spec.js
@@ -1,55 +1,57 @@
 'use strict';
 
-var assert = require('assert'),
-    gitignoreToGlob = require('../lib/gitignore-to-glob');
+const assert = require('assert');
+const gitignoreToGlob = require('../lib/gitignore-to-glob');
 
-function getProcessedArray(gitignoreSuffixOrArray, dirsToCheck) {
-    return Array.isArray(gitignoreSuffixOrArray) ?
+const getProcessedArray = (gitignoreSuffixOrArray, dirsToCheck) =>
+    Array.isArray(gitignoreSuffixOrArray) ?
         gitignoreSuffixOrArray :
-        gitignoreToGlob(__dirname + '/fixtures/gitignore-' + gitignoreSuffixOrArray, dirsToCheck);
-}
+        gitignoreToGlob(
+            `${ __dirname }/fixtures/gitignore-${ gitignoreSuffixOrArray }`,
+            dirsToCheck
+        );
 
-function assertContain(gitignoreSuffixOrArray, elem) {
-    var processedArray = getProcessedArray(gitignoreSuffixOrArray);
+const assertContain = (gitignoreSuffixOrArray, elem) => {
+    const processedArray = getProcessedArray(gitignoreSuffixOrArray);
     return assert.notEqual(processedArray.indexOf(elem), -1,
-        'Expected ' + JSON.stringify(processedArray) + ' to contain ' + elem);
-}
+        `Expected ${ JSON.stringify(processedArray) } to contain ${ elem }`);
+};
 
-function assertNotContain(gitignoreSuffixOrArray, elem) {
-    var processedArray = getProcessedArray(gitignoreSuffixOrArray);
+const assertNotContain = (gitignoreSuffixOrArray, elem) => {
+    const processedArray = getProcessedArray(gitignoreSuffixOrArray);
     return assert.equal(processedArray.indexOf(elem), -1,
-        'Expected ' + JSON.stringify(processedArray) + ' to not contain ' + elem);
-}
+        `Expected ${ JSON.stringify(processedArray) } to not contain ${ elem }`);
+};
 
-function assertDeep(gitignoreSuffixOrArray, expectedArray) {
-    var processedArray = getProcessedArray(gitignoreSuffixOrArray);
+const assertDeep = (gitignoreSuffixOrArray, expectedArray) => {
+    const processedArray = getProcessedArray(gitignoreSuffixOrArray);
     return assert.deepEqual(processedArray, expectedArray);
-}
+};
 
-describe('gitignoreToGlob', function () {
-    it('should treat paths starting with "/" as relative to the main directory', function () {
+describe('gitignoreToGlob', () => {
+    it('should treat paths starting with "/" as relative to the main directory', () => {
         assertContain('slash', '!pattern');
     });
 
-    it('should treat paths not starting with "/" as relative to all directories', function () {
+    it('should treat paths not starting with "/" as relative to all directories', () => {
         assertContain('noslash', '!**/pattern');
     });
 
-    it('should not ignore files ignored in .gitignore', function () {
+    it('should not ignore files ignored in .gitignore', () => {
         assertContain('ignore', '**/pattern1');
         assertContain('ignore', 'pattern2');
     });
 
-    it('should omit empty lines', function () {
+    it('should omit empty lines', () => {
         assertDeep('emptyline', ['!pattern1', '!pattern1/**', '!pattern2', '!pattern2/**']);
     });
 
-    it('should omit comments', function () {
+    it('should omit comments', () => {
         assertDeep('comment', ['!pattern1', '!pattern1/**', '!pattern2', '!pattern2/**']);
     });
 
-    it('should take `dirsToCheck` into account', function () {
-        var processedArray = getProcessedArray('dirs', ['pattern1', 'pattern3']);
+    it('should take `dirsToCheck` into account', () => {
+        const processedArray = getProcessedArray('dirs', ['pattern1', 'pattern3']);
         assertContain(processedArray, '!pattern1');
         assertNotContain(processedArray, '!pattern2');
         assertContain(processedArray, '!pattern3');
